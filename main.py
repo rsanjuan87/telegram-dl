a6a105d0bcd9bf304e165ba7e78af71360b67d7e
San Juan <rsanjuan1987@gmail.com>
2023-07-05 12:31:47 +0200

    added show download progress added output dir added store credentials on config dir

--- main.py
+++ main.py
@@ -5,20 +5,36 @@ from telethon.tl.types import InputChannel,  MessageMediaDocument, DocumentAttri
 from telethon.errors import SessionPasswordNeededError
 import sys
 import os
+from pathlib import Path
+from tqdm import tqdm
 
 api_id = 21518307
 api_hash = '0c3d16f2b71e5be100746c6b78828963'
+app_name = 'telegram-dl'
+
+config_dir = '{}/.config/{}/'.format(Path.home(), app_name)
+if not os.path.exists(config_dir):
+    os.mkdir(config_dir)
 
 if len(sys.argv) > 1:
     url = sys.argv[1]
 else:
     print("Please provide a URL")
     sys.exit(1)
+output_dir = '.'
+if len(sys.argv) > 2:
+    output_dir = sys.argv[2]
 
 phone_number = ''
-files = [f for f in os.listdir('.') if f.endswith('.session')]
+files = [f for f in os.listdir(config_dir) if f.endswith('.session')]
+
+def progress_callback(downloaded_bytes, total_bytes):
+    # print(f"Descargado {downloaded_bytes} de {total_bytes} bytes: {downloaded_bytes / total_bytes:.2%}")
+    bar.update(downloaded_bytes - bar.n)
+
 
 if len(files) == 0:
+    print("No sessions found, please login")
     phone_number = input("Phone Number: ")
 elif len(files) == 1:
     phone_number = files[0].removesuffix('.session')
@@ -37,7 +53,7 @@ else:
 channel = url.split('/')[3]
 message_id = int(url.split('/')[4])
 
-client = TelegramClient(phone_number, api_id, api_hash)
+client = TelegramClient('{}{}'.format(config_dir, phone_number), api_id, api_hash)
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone_number)
@@ -73,8 +89,9 @@ if message.media:
         for attribute in message.media.document.attributes:
             if isinstance(attribute, DocumentAttributeFilename):
                 filename = attribute.file_name
-                print('Filename:', filename)
-    message.download_media(filename)
-    print('Finish: {}'.format(filename)
+    print('Filename:', filename, ', Output directory:', output_dir)
+    print('Downloading...')
+    with tqdm(total=message.media.document.size, unit='B', unit_scale=True, ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}') as bar:
+        client.download_media(message.media, '{}/{}'.format(output_dir, filename), progress_callback = progress_callback)
 
 sys.exit(0)
